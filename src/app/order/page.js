3f24dd91f7e69b78cf0dfb46a4f57608b971b9a6
AMANULLAH023HY <amana170829@gmail.com>
2024-07-26 15:32:01 +0530

    place order now page

--- src/app/order/page.js
+++ src/app/order/page.js
@@ -4,9 +4,12 @@ import { useState } from "react";
 import CustomerHeader from "../_components/CustomerHeader";
 import Footer from "../_components/Footer";
 import { DELIVERY_CHARGES, TAX } from "../lib/constant";
-import { useRouter } from "next/navigation";
 
 const page = () => {
+  const [userStorage, setUserStorage] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
+
   const [cartStorage, setCartStorage] = useState(
     JSON.parse(localStorage.getItem("cart"))
   );
@@ -19,45 +22,29 @@ const page = () => {
         })
   );
 
-
-const router = useRouter();
-
-const orderNow =()=>{
-  router.push('http://localhost:3000/order')
-}
+  console.log(total);
 
   return (
     <div>
       <CustomerHeader />
-      <div>
-        {cartStorage.map((item) => (
-          <div>
-            <div className="flex m-3 ">
-              <img
-                className="w-[200px] h-[200px] rounded-sm "
-                src={item.img_path}
-                alt="food iamge"
-              />
-              <div className="m-2 font-semibold">
-                <p>{item.foodname}</p>
-                <p className="font-light">{item.description}</p>
-                {
-                  <button
-                    onClick={() => removeFromCart(item._id)}
-                    className="rounded-md p-3 mt-5  bg-green-600 text-white font-bold"
-                  >
-                    Remove From Cart
-                  </button>
-                }
-              </div>
-              <p className="font-bold m-3 text-end">Price : {item.price}</p>
-            </div>
 
-            <div className="h-[2px] bg- bg-sky-500 w-full"></div>
-          </div>
-        ))}
-      </div>
       <div className="flex flex-col flex-wrap mt-4">
+        <div className="flex flex-col flex-wrap mt-4">
+          <h2 className="text-center font-bold text-2xl">User Details</h2>
+          <div className="flex justify-between ml-[20%] font-bold text-[20px] mr-[20%] ">
+            <span>Name </span>
+            <span>{userStorage.name} </span>
+          </div>
+          <div className="flex justify-between ml-[20%] font-bold text-[20px] mr-[20%] ">
+            <span>Address </span>
+            <span>{userStorage.address} </span>
+          </div>
+          <div className="flex justify-between ml-[20%] font-bold text-[20px] mr-[20%] ">
+            <span>Mobile </span>
+            <span>{userStorage.phone} </span>
+          </div>
+        </div>
+
         <div className="flex justify-between ml-[20%] font-bold text-[20px] mr-[20%] ">
           <span>Food Charge : </span>
           <span>{total} </span>
@@ -78,9 +65,16 @@ const orderNow =()=>{
           <span>{total + DELIVERY_CHARGES + (total * TAX) / 100} </span>
         </div>
       </div>
+
+      <h1 className="text-center font-bold text-2xl">Payment Mode</h1>
+      <div className="flex justify-between ml-[20%] font-bold text-[20px] mr-[20%] ">
+        <span>Cash on Delivery : </span>
+        <span>{total + DELIVERY_CHARGES + (total * TAX) / 100} </span>
+      </div>
+
       <div className="flex justify-center mt-3 ">
-        <button onClick={orderNow} className="p-2 w-56 rounded-xl font-bold text-2xl bg-orange-500 ">
-          Order Now
+        <button className="p-2 w-auto rounded-xl font-bold text-2xl bg-orange-500 ">
+          Place Your Order Now
         </button>
       </div>
 
